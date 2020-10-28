e1267011e2cb987e1869aa85fdf5dab317cc5410
mv343203 <mvome@outlook.com>
2020-10-28 12:39:20 -0400

    Add expense summary to dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpensesSummary } from '../../components/ExpenseSummary';
+
+
+test('should correctly render ExpensesSummary with 1 expense', () =>{
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={235}/>);
+    expect(wrapper).toMatchSnapshot();
+});
+
+
+test('should correctly render ExpensesSummary with multiple expenses', () =>{
+    const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={23543432434453}/>);
+    expect(wrapper).toMatchSnapshot();
+});
