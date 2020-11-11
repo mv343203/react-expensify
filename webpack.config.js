a342ed8e72d590ce393ef058f447f01ccbd657de
mv343203 <mvome@outlook.com>
2020-11-11 06:11:54 -0500

    Setup test database environment

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,16 @@ const webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'test'){
+if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test'});
- } else if (process.env.NODE_ENV === 'development'){
-  require('dotenv').config({ path: '.env.development'});
+ } else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' });
   }
 
 
 
 module.exports = (env) => {
-  const isProduction = env ==='production';
+  const isProduction = env === 'production';
   const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
   return {
