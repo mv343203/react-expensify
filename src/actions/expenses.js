3237be0e25a058d8ff3b128d0c2042456b1f8c0c
mv343203 <mvome@outlook.com>
2020-11-17 16:08:23 -0500

    Add start edit expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -46,11 +46,23 @@ export const startRemoveExpense = ({ id } = {}) => {
     };
 };
 
+//EDIT_EXPENSE
+
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+export const startEditExpense= (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates));
+        });
+    };
+}
+
+
 
 //SET_EXPENSES
 
